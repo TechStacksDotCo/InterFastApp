29f0454ce70053a535939f46f2b942896943e272
marcjohnboanas <marc.john.boanas@gmail.com>
2021-09-04 12:08:31 +0100

    Initial Commit

---
+++ InterFastApp/InterFastAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  InterFastAppApp.swift
+//  InterFastApp
+//
+//  Created by Marc Boanas on 04/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct InterFastAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
